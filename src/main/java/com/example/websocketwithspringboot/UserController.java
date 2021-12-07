def61bdf016a1a84ba4c4797100890bc3dc4e51c
wannaberoot <kamilozan35@gmail.com>
2021-12-08 02:09:26 +0300

    UserController class added

---
+++ src/main/java/com/example/websocketwithspringboot/UserController.java
@@ -0,0 +1,63 @@
+package com.example.websocketwithspringboot;
+
+import org.aspectj.weaver.Iterators;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        try {
+            List<User> users = new ArrayList<>();
+
+            if (users.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                userRepository.findAll();
+            }
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<User> createUser(@RequestParam String userName, @RequestParam String phoneNumber) {
+        try {
+            User user = new User();
+            user.setUserName(userName);
+            user.setPhoneNumber(phoneNumber);
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/deleteUserById")
+    public ResponseEntity<HttpStatus> deleteUserById(@RequestParam long id) {
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
