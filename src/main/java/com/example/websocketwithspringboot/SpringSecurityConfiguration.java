23d27945fac9c76c8a5aab7249cfdf2c97cf30f7
wannaberoot <kamilozan35@gmail.com>
2021-12-08 04:14:30 +0300

    Spring Boot Security configuration for both api and database

---
+++ src/main/java/com/example/websocketwithspringboot/SpringSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.websocketwithspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SpringSecurityConfiguration(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().
+                authorizeRequests().antMatchers("/users").hasRole("ADMIN")
+                .antMatchers("/deleteUserById)").hasRole("ADMIN")
+                .antMatchers("/createUser").permitAll()
+                .and().httpBasic();
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+}
