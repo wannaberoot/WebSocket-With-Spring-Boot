44bf8014056e0316d818cd7ee157fb837caaa40d
wannaberoot <kamilozan35@gmail.com>
2021-12-08 03:52:08 +0300

    UserController and repository changed

--- src/main/java/com/example/websocketwithspringboot/UserController.java
+++ src/main/java/com/example/websocketwithspringboot/UserController.java
@@ -1,14 +1,11 @@
 package com.example.websocketwithspringboot;
 
-import org.aspectj.weaver.Iterators;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api")
--- src/main/java/com/example/websocketwithspringboot/UserRepository.java
+++ src/main/java/com/example/websocketwithspringboot/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.websocketwithspringboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findUsersById(Long id);
-}
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
