94622c3a631168010053338665388b2e68be7c3f
wannaberoot <kamilozan35@gmail.com>
2021-12-09 05:46:21 +0300

    Security config added

--- src/main/java/com/example/websocketwithspringboot/WebSocketWithSpringBootApplication.java
+++ src/main/java/com/example/websocketwithspringboot/WebSocketWithSpringBootApplication.java
@@ -1,9 +1,16 @@
 package com.example.websocketwithspringboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "user-api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "User Operations and Details"))
 public class WebSocketWithSpringBootApplication {
 
     public static void main(String[] args) {
