e5de57c5cf64e09ce4b4458f775433fb71509689
wannaberoot <kamilozan35@gmail.com>
2021-12-11 02:04:32 +0300

    Base message controller and UI created for general purpose

---
+++ src/main/java/com/example/websocketwithspringboot/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.example.websocketwithspringboot;
+
+public class ResponseMessage {
+
+    private String content;
+
+    public ResponseMessage() {};
+
+    public ResponseMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {this.content = content;}
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,50 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#messages").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/WebSocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/messages', function (message) {
+            showMessages(JSON.parse(message.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendNumber() {
+    stompClient.send("/app/message", {}, JSON.stringify({'messageContent': $("#number").val()}));
+}
+
+function showMessages(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendNumber(); });
+});
