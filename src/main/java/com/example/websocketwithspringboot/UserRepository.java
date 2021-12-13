0bfaec366a43fbe028cc31cfa6cc1e53076f639f
wannaberoot <kamilozan35@gmail.com>
2021-12-13 05:15:13 +0300

    Deleted classes that are no longer needed

--- src/main/java/com/example/websocketwithspringboot/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.websocketwithspringboot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {}
