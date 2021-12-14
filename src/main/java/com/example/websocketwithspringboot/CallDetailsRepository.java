9546b1a185c6bffb65d7fc177269cd6374339512
wannaberoot <kamilozan35@gmail.com>
2021-12-14 14:30:43 +0300

    Some improvements about repository methods

--- src/main/java/com/example/websocketwithspringboot/CallDetailsRepository.java
+++ src/main/java/com/example/websocketwithspringboot/CallDetailsRepository.java
@@ -2,9 +2,12 @@ package com.example.websocketwithspringboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CallDetailsRepository extends JpaRepository<CallDetails, Integer> {
 
-    CallDetails findCallByToWhom(String phoneNumber);
+    List<CallDetails> findCallByToWhom(String phoneNumber);
+
+    List<CallDetails> findCallByToWhomAndFromWho(String toWhom, String fromWho);
 }
